3c7560c429616b0c2a2368639dcb1d4481829624
lauboom <52071436+lauboom@users.noreply.github.com>
2021-09-07 23:07:17 +0800

    Update xray-wp.sh

--- xray-wp.sh
+++ xray-wp.sh
@@ -453,6 +453,7 @@ EOF
     systemctl enable xray.service
     sed -i "s/User=nobody/User=root/;" /etc/systemd/system/xray.service
     systemctl daemon-reload
+    ~/.acme.sh/acme.sh --register-account -m lytommy@live.com
     ~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
         --key-file   /usr/local/etc/xray/cert/private.key \
         --fullchain-file  /usr/local/etc/xray/cert/fullchain.cer \
